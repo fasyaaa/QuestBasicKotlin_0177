e58fdf8405cefa086751d4754d88e13270fdbedf
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 06:09:30 +0700

    File tipe data selesai

---
+++ app/src/main/java/com/example/pam1/TipeData.kt
@@ -0,0 +1,46 @@
+package com.example.pam1
+
+import android.text.BoringLayout
+
+fun main() {
+
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Short
+    val shortValue: Short = 20
+    println("Shory: $shortValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    // Float (Data Harus menggunakan f pada akhiran)
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("String: $stringValue")
+
+}
