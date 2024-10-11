0ff8cf75e392ce6e33aeb07321a60e9ab17837db
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 06:18:24 +0700

    pembuatan fungsi untuk menampilkan hanya email dibuat

--- app/src/main/java/com/example/pam1/Class.kt
+++ app/src/main/java/com/example/pam1/Class.kt
@@ -1,2 +1,13 @@
 package com.example.pam1
 
+class Motor()
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+//    Prints the value of the properties: email
+    println(contact.email)
+
+}
