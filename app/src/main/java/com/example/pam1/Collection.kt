6a2bb0ce2d4e3f444fa37f0b97f210fee7f427d1
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 06:32:26 +0700

    list Read Only sudah selesai

--- app/src/main/java/com/example/pam1/Collection.kt
+++ app/src/main/java/com/example/pam1/Collection.kt
@@ -1,2 +1,14 @@
 package com.example.pam1
 
+// List adalah Kumpulan data yang disusun secara berurutan
+// List bersifat ordered, artinya data yang dimasukkan akan memilki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("====== List ======")
+//    List Read Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+}
