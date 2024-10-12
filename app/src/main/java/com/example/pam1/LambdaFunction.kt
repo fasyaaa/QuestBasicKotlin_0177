3bafa9ad1d2d2f783d592f38cdec831e8d9fed6f
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 20:39:11 +0700

    file LambdaFunction.kt sudah selesai

--- app/src/main/java/com/example/pam1/LambdaFunction.kt
+++ app/src/main/java/com/example/pam1/LambdaFunction.kt
@@ -1,2 +1,18 @@
 package com.example.pam1
 
+// Lambda Function
+// Lambda Function adalah fungsi yang tidak memiliki nama
+// Lambda Function biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Function menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun upperCaseString(string: String): String {
+    return string.uppercase()
+}
+
+// Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    upperCaseString("hello")
+    println({
+        string: String -> string.uppercase()
+    }("hello"))
+}
