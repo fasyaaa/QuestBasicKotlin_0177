46a626273be233466f7fac952582d9b00914d128
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 07:48:45 +0700

    menambah dan menghapus data pada bagian Map Mutable

--- app/src/main/java/com/example/pam1/Collection.kt
+++ app/src/main/java/com/example/pam1/Collection.kt
@@ -48,6 +48,16 @@ fun ContohMap() {
 // Map Mutable
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(shape)
+
+//    Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println()
+
+//    Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+
 }
 
 fun main(){
