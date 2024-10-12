f578924328b676d446eeb35e29b7a17cc7abaefe
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 07:24:02 +0700

    Menghapus data ke dalam list mutable

--- app/src/main/java/com/example/pam1/Collection.kt
+++ app/src/main/java/com/example/pam1/Collection.kt
@@ -19,4 +19,10 @@ fun ContohList() {
 //    Menambahkan data ke dalam List Mutable
     shape.add("Circle")
     println(shape)
+
+//    Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+
 }
