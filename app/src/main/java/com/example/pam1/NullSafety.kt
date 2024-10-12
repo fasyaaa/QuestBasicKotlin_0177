0bbf883abe5c2dc7a53830a0b14665c0082b5892
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 20:46:26 +0700

    file NullSafety.kt telah selesai

--- app/src/main/java/com/example/pam1/NullSafety.kt
+++ app/src/main/java/com/example/pam1/NullSafety.kt
@@ -1,2 +1,40 @@
 package com.example.pam1
 
+import org.jetbrains.annotations.NotNull
+
+fun nullSafety(){
+//    neverNull has String type
+    var neverNull: String = "This can't be null"
+
+//    Throws a compiler eror
+//    neverNull = null
+
+//    nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+//    This is OK
+    nullable = null
+
+
+//    Check for null in conditions
+if (neverNull == null){
+    println("inferredNotNull is null")
+} else{
+    println("inferredNotNull is not null")
+}
+
+//    Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+
+//    notNull doesn't accept null values
+    fun strLength(notNull: String): Int{
+        return notNull.length
+    }
+
+    println(strLength(neverNull))   //18
+}
+
+fun main() {
+    nullSafety()
+}
