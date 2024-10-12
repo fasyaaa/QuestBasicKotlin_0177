68c0fb7175e968831bd6110430d3e8a72feb2976
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-10-12 20:28:40 +0700

    file Function.kt sudah selesai

--- app/src/main/java/com/example/pam1/Function.kt
+++ app/src/main/java/com/example/pam1/Function.kt
@@ -1,2 +1,36 @@
 package com.example.pam1
 
+fun withoutParameter() {
+    println("== Without Parameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println("== With Parameter ==")
+    println("Hello, $name!")
+}
+
+// Named argument
+// Named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int){
+    println("== With Named Argument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// Default parameter value
+// Default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Adifa", age: Int) {
+    println("== With Default Parameter")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang: Int, lebar: Int){
+    return println(panjang * lebar)
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
